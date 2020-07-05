63d1527bbc2f4a1182ff227ec574813cbefd737a
osteenw <58855696+osteenw@users.noreply.github.com>
2020-07-05 18:03:50 -0400

    Update

--- app.py
+++ app.py
@@ -2,20 +2,31 @@ import numpy as np
 from flask import Flask, request, jsonify, render_template, redirect, url_for, session, flash
 import pickle
 import pandas as pd
+from functools import wraps
 
 app = Flask(__name__)
 
 app.secret_key = "2FABF43CA4C11CA916817BB689461"
 
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('You need to login first.')
+            return redirect(url_for('login'))
+    return wrap
 
 @app.route('/')
 def home():
-    if session == True:
+    if 'logged_in' in session:
         return redirect(url_for('welcome'))
     else:
         return redirect(url_for('login'))
 
 @app.route('/index')
+@login_required
 def welcome():
     return render_template('index.html')
 
@@ -26,12 +37,13 @@ def login():
         if request.form['username'] != 'test' or request.form['password'] != 'test':
             error = 'Invalid credentials. Please try again.'
         else:
-            session['logged in'] = True
+            session['logged_in'] = True
             # flash('You were just logged in!')
             return redirect(url_for('welcome'))
     return render_template('login.html', error=error)
 
 @app.route('/logout')
+@login_required
 def logout():
     session.pop('logged_in', None)
     flash('You were just logged out!')
@@ -73,12 +85,6 @@ def predict():
 
         model = pickle.load(open('random_forst_model_1.pkl', 'rb'))
         prediction = model.predict(new_vector.reshape(1, -1))
-
-    # int_features = [x for x in request.form.values()]
-    # final = np.array(int_features)
-    # data_unseen = pd.DataFrame([final], columns = cols)
-    # prediction = predict_model(model, data=data_unseen, round = 0)
-    # prediction = int(prediction.Label[0])
 
     return render_template('index.html',pred='Expected Bill will be ${:.2f}'.format(prediction[0]))
 
